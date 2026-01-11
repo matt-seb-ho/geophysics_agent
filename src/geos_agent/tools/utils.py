ed922aa07a7f4b14fe6fc783deb74334c6e4597e
brianzliu <brianliu0317@gmail.com>
2026-01-10 16:36:33 -0800

    fix: geos-agent only has access to data/ dir

--- src/geos_agent/tools/utils.py
+++ src/geos_agent/tools/utils.py
@@ -9,13 +9,19 @@ from .shell_tools import ListDirTool, PythonExecTool, ShellCommandTool
 
 
 def build_default_tools(workspace_root: Path) -> List[Tool]:
+    """Build the default set of tools for the agent.
+    
+    File and shell tools are restricted to the data/ subdirectory
+    to prevent the agent from modifying source code or other files.
+    """
+    data_root = workspace_root / "data"
     return [
-        ReadFileTool(workspace_root),
-        WriteFileTool(workspace_root),
-        ListDirTool(workspace_root),
-        ShellCommandTool(workspace_root),
-        PythonExecTool(workspace_root),
+        ReadFileTool(data_root),
+        WriteFileTool(data_root),
+        ListDirTool(data_root),
+        ShellCommandTool(data_root),
+        PythonExecTool(data_root),
         SearchGeosDocsTool(),
         SearchWebTool(),
-        RunGeosTool(workspace_root),
+        RunGeosTool(data_root),
     ]
